4ca3b7158b584adaad98283d8110c21538412730
kylecuberg <kyle.patterson@cuberg.net>
2023-03-27 10:09:05 -0700

    add single cell printing

---
+++ src/single_cell_workorder.py
@@ -0,0 +1,40 @@
+# Standard library
+import os
+
+# First-party/Local
+import private
+import util
+
+if __name__ == "__main__":
+    """[summary]"""
+
+    try:
+        # get from sql
+        sparc = util.MySQL(
+            os.getenv("mysql_user", private.mysql_user),
+            os.getenv("mysql_password", private.mysql_password),
+            os.getenv("mysql_host", private.mysql_host),
+            database=os.getenv("mysql_database", "sparc"),
+        )
+
+        cell_list = "".join(str(input("Please type in Work Order to print labels for: ")).strip())
+
+        for cell in [cell_list]:
+            cell = str(cell[0])
+
+            cell_list = sparc.select(
+                rf"""select thingname, workorder from sparc.thing where thingname like '{cell}' order by thingname desc"""
+            ).values.tolist()
+
+            for row in cell_list:
+                cell = row[0]
+                workorder = row[1]
+
+                label = util.qr_text(label_x=2, label_y=1, dpi=os.getenv("zt411_dpi", private.zt411_dpi))
+                z = util.zebra(qr=label.sn_wo(cell=cell, workorder=workorder))
+                z.send(
+                    host=os.get_env("zt411_host", private.zt411_host), port=os.get_env("zt411_port", private.zt411_port)
+                )
+
+    except Exception as E:
+        print(E, type(E).__name__, __file__, E.__traceback__.tb_lineno)
