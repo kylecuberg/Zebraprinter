18ff00ac9a42972a39e9d5ac1c957a6876cf8b90
kylecuberg <kyle.patterson@cuberg.net>
2023-03-27 09:58:01 -0700

    update scripts

--- input/Print WorkOrder.bat
+++ input/Print WorkOrder.bat
@@ -1,3 +1,3 @@
 @echo off
-C:\Users\KylePatterson\AppData\Local\Programs\Python\Python310\python.exe "%~dp0..\src\wo.py"
+C:\Users\KylePatterson\AppData\Local\Programs\Python\Python310\python.exe "%~dp0..\src\wo_workorder.py"
 pause
